497a5fecd4dab866a262266a351e51a8a445f141
fayas122 <mfayas122@gmail.com>
2026-06-20 09:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (25.34%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let n = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[n] = nums[i];
+            n++;
+        }
+    }
+    return n;
+};
