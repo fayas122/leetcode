1f75066831ff60b0e6c85533314fcdd0da0bbdb8
fayas122 <mfayas122@gmail.com>
2026-07-27 09:52:42 +0530

    Time: 2 ms (55.36%) | Memory: 54 MB (69.53%) - LeetSync

---
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+      let first = 0;
+    let second = 0;
+
+    for (let num of nums) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second) {
+            second = num;
+        }
+    }
+
+    return (first - 1) * (second - 1);
+};
