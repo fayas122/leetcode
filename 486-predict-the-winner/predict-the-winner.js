b21515914f7ad6b6c1c231b2b0303bcebf982892
fayas122 <mfayas122@gmail.com>
2026-08-01 14:07:46 +0530

    Time: 1 ms (86.89%) | Memory: 53.8 MB (72.13%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var predictTheWinner = function(nums) {
+     const n = nums.length;
+
+    const dp = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        dp[i][i] = nums[i];
+    }
+
+    for (let len = 2; len <= n; len++) {
+        for (let i = 0; i <= n - len; i++) {
+            const j = i + len - 1;
+
+            const left = nums[i] - dp[i + 1][j];
+            const right = nums[j] - dp[i][j - 1];
+
+            dp[i][j] = Math.max(left, right);
+        }
+    }
+
+    return dp[0][n - 1] >= 0;
+};
