a60022790272e228d359017c599330c534c77394
fayas122 <mfayas122@gmail.com>
2026-08-04 09:35:35 +0530

    Time: 3 ms (64.57%) | Memory: 58.3 MB (64.57%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+
+    const set = new Set(nums);
+    const result = [];
+
+    for (let i = min; i <= max; i++) {
+        if (!set.has(i)) {
+            result.push(i);
+        }
+    }
+    return result;
+};
